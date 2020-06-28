d200c849701ddcbf778c30ea3884a1e0138a78d1
codingpalette <codingpalette@gmail.com>
2020-06-28 14:16:30 +0900

    admin 페이지 생성

---
+++ front3/pages/admin/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from "../../components/Layout";
+
+
+const Admin = () => {
+    return(
+        <>
+            <Layout>
+                sfsfs
+            </Layout>
+        </>
+    )
+}
+
+export default Admin
---
+++ front3/pages/admin/post_list.js
@@ -0,0 +1,163 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import Link from 'next/link';
+import Router from "next/router";
+import {useDispatch, useSelector} from "react-redux";
+import {LOAD_POSTS_REQUEST, RESET_SUCCESS} from "../../reducers/post";
+import Layout from "../../components/Layout";
+import styled from '@emotion/styled';
+
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
+
+
+const ContentHeader = styled.div`
+    background-color: #fff;
+    padding: 2rem 1rem;
+    box-sizing: border-box;
+    & h2{
+       font-size: 2.5rem;
+       font-weight: bold;
+    }
+    & .link_box{
+        text-align: right;
+    }
+`;
+
+
+const Container = styled.div`
+    padding: 1rem;
+    box-sizing: border-box;
+    & .table_content{
+        width: 100%;
+        background-color: #fff;
+        border-radius: 4px;
+        box-shadow: 0 .5rem 1rem rgba(0,0,0,.15)!important;
+    }
+    & .table_content table{
+        width: 100%;
+    }
+    & .table_content thead th{
+        padding: 1rem;
+        font-size: 12px;
+        text-align: left;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+    }
+    & .table_content tbody tr{
+        position: relative;
+    }
+    & .table_content tbody tr:hover {
+        background: #f5f5f5;
+    }
+    & .table_content tbody td {
+        color: rgba(0, 0, 0, 0.54);
+        font-size: 13px;
+        padding: 0.5rem 1rem;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.06);
+        white-space: nowrap;
+    }
+    & .table_content tbody td .btn_box{
+        text-align: right;
+        position: relative;
+    }
+`;
+
+
+const postList = () => {
+    const dispatch = useDispatch();
+    const { mainPosts, hasMorePosts, loadPostsLoading } = useSelector((state) => state.post);
+
+    useEffect(() => {
+        if (mainPosts.length === 0) {
+            dispatch({
+                type: LOAD_POSTS_REQUEST
+            })
+        }
+    }, [])
+
+    useEffect(() => {
+        dispatch({
+            type: RESET_SUCCESS
+        })
+    })
+
+    const [postMenu, setPostMenu] = useState(null);
+    const onClickPostMenuOpen = useCallback((event) => {
+        setPostMenu(event.currentTarget);
+    }, []);
+    const onClickPostMenuClose = useCallback(() => {
+        setPostMenu(null);
+    }, []);
+    const onClickPostModify = useCallback((id) => () => {
+        Router.push(`/write?id=${id}`)
+    }, [])
+
+    return(
+        <>
+            <Layout>
+                <ContentHeader>
+                    <h2>포스트 리스트</h2>
+                    <div className="link_box">
+                        <Link href="/write" >
+                            <a>
+                                <Button color="primary">포스트 작성</Button>
+                            </a>
+                        </Link>
+                    </div>
+                </ContentHeader>
+                <Container>
+                    <div className="table_content">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>제목</th>
+                                    <th> </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {mainPosts.length > 0 && mainPosts.map(post => (
+                                    <tr key={post.id}>
+                                        <td>
+                                            <div>
+                                                <Link href="/post/[id]" as={`/post/${post.id}`}>
+                                                    <a>{post.title}</a>
+                                                </Link>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="btn_box">
+                                                <>
+                                                    <IconButton aria-label="settings" onClick={onClickPostMenuOpen}>
+                                                        <MoreVertIcon />
+                                                    </IconButton>
+                                                    <Menu
+                                                        anchorEl={postMenu}
+                                                        keepMounted
+                                                        open={Boolean(postMenu)}
+                                                        onClose={onClickPostMenuClose}
+                                                    >
+                                                        <MenuItem onClick={ onClickPostModify(post.id)}>수정하기</MenuItem>
+                                                        <MenuItem onClick={onClickPostMenuClose}>삭제하기</MenuItem>
+                                                    </Menu>
+                                                </>
+
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </Container>
+            </Layout>
+        </>
+    )
+}
+
+export default postList;
