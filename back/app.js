3585baf438da6e91037142bb031c3222e1aa6606
codingpalette <codingpalette@gmail.com>
2020-06-30 17:48:54 +0900

    back í 폴더 생성

---
+++ back/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+
+
+app.listen(5000, () => {
+    console.log('서버실행중')
+})
