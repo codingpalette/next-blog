767e1c2b78f6f02de6739c2ebf4b0fa6dfb109fb
codingpalette <codingpalette@gmail.com>
2020-08-04 14:02:23 +0900

    pm2 install

--- back/app.js
+++ back/app.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path = require('path');
+const hpp = require('hpp');
+const helmet = require('helmet');
+
 
 
 const userRouter = require('./routes/user');
@@ -30,7 +33,14 @@ db.sequelize.sync()
 
 passportConfig();
 
-app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(hpp());
+    app.use(helmet());
+} else {
+    app.use(morgan('dev'))
+}
+
 app.use(cors({
     origin: true, // 나중에는 실제 프론트 주소를 넣어야함
     credentials: true, // 서로 다른 도메인간 쿠기 보내기 허용
