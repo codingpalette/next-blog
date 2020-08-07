23290d1e177e78fa0de5473c2e668737a5dd8b30
codingpalette <codingpalette@gmail.com>
2020-08-07 22:00:02 +0900

    lambda image resize

---
+++ lambda/index.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const s3 = new AWS.S3()
+
+
+exports.handler = async (event, context, callback) => {
+    const Bucket = event.Records[0].s3.bucket.name; // s3 이름 (codingpalette)
+    const Key = event.Records[0].s3.object.key;
+    const filename = Key.split('/')[Key.split('/').length -1];
+    const ext = Key.split('.')[Key.split('.').length -1];
+    const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
+
+    try {
+        const s3Object = await s3.getObject({ Bucket, Key }).promise();
+        const resizedImage = await sharp(s3Object.Body)
+            .resize(1000, 1000, { fit:'inside' })
+            .toFormat(requiredFormat)
+            .toBuffer()
+        await s3.putObject({
+            Bucket,
+            Key: `thumb/${filename}`,
+            Body: resizedImage
+        }).promise()
+
+        return callback(null, `thumb/${filename}`)
+    } catch(e) {
+        return callback(e)
+    }
+}
