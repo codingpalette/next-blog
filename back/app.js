53ae449c270be834bd5d2f8981292c55a8c81ea2
codingpalette <codingpalette@gmail.com>
2020-08-07 15:11:26 +0900

    back session cookie create

--- back/app.js
+++ back/app.js
@@ -58,6 +58,11 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        domain: process.env.NODE_ENV === 'production' && '.codingpalette.com'
+    },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
