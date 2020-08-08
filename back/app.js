6a70256ee61d560946e817681f30288bb37cd565
codingpalette <codingpalette@gmail.com>
2020-08-08 13:24:35 +0900

    back app.js modify

--- back/app.js
+++ back/app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(hpp());
     app.use(helmet());
     app.use(cors({
-        origin: '*',
+        origin: true,
         credentials: true,
     }));
 } else {
