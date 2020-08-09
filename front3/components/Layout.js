6b2023859ffd3deec41e5ffe59742a51da28aa12
codingpalette <codingpalette@gmail.com>
2020-08-09 14:18:49 +0900

    layout min-height

--- front3/components/Layout.js
+++ front3/components/Layout.js
@@ -6,7 +6,7 @@ import styled from '@emotion/styled';
 
 const ContainerBox = styled.div`
     width: 100%;
-    //height: 100vh;
+    min-height: 100vh;
     display: flex;
     align-items: flex-start;
     flex-direction: column;
